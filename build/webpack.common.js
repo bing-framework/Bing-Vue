736011fe1a73e3d28c37f2534941499a5dd0456c
jian玄冰 <jianxuanhuo1@126.com>
2018-03-28 01:04:01 +0800

    修改配置文件

--- build/webpack.common.js
+++ build/webpack.common.js
@@ -5,3 +5,59 @@ const webpack = require('webpack')
 
 // webpack plugins
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+
+module.exports = function(options) {
+    return {
+        context:path.resolve(__dirname,'../'),
+        entry: {
+            app:'./src/main.ts'
+        },
+        output: {
+            
+        },
+        resolve: {
+            extensions: ['.js','.vue','.json','.ts'],
+            alias: {
+                'vue$': 'vue/dist/vue.esm.js',
+                '@':resolve('src'),
+            }
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.vue$/,
+                    loader: 'vue-loader',                    
+                },
+                {
+                    test: /\.tsx?$/,
+                    exclude: /node_modules/,
+                    use: [
+                        "babel-loader",
+                        {
+                            loader: "ts-loader",
+                            options: { 
+                                appendTsxSuffixTo: [/\.vue$/] 
+                            }
+                        }
+                    ]
+                },
+                {
+                    test: /\.js$/,
+                    loader: 'babel-loader',
+                    include:[
+                        resolve('src'),
+                        resolve('test'),
+                    ]
+                },
+                {
+                    test:  /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+                    loader: 'url-loader',
+                    options: {
+                        limit: 10000                        
+                    }
+                }
+            ]
+        }
+    }
+}
